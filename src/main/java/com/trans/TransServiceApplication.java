4dc1b8eadf5708cf991aae0ebe9096674cdf0bf7
Denny Afrizal <denny.afrizal@alfadigital.id>
2020-10-23 13:20:33 +0700

    Project Skeleton

---
+++ src/main/java/com/trans/TransServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransServiceApplication.class, args);
+	}
+
+}
